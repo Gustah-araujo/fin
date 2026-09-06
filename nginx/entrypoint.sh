b90fe0bfcb696de65bdf1b253fd1df874569ba1d
Gustavo Araújo <gustavoc.araujo2001@gmail.com>
2026-09-06 18:27:32 -0300

    fix: rename app.conf to template to prevent direct nginx load
    nginx loads *.conf from conf.d — template with  literal
    causes startup failure. Only default.conf (envsubst'd) should load.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/app.conf > /etc/nginx/conf.d/default.conf
+envsubst '${APP_DOMAIN}' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
